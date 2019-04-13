7b08f6e2f9c936911fc4bb35545a3dfd586a195f
bstrie <bstrie@thin>
2019-04-13 17:41:23 -0400

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+pub struct Foo(i32);
+
+impl Add for Foo {
+    type Output = Foo;
+    fn add(self, other: Foo) -> Foo {
+        Foo(self.0 + other.0)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn add() {
+        let bar = Foo(1);
+        let qux = Foo(2);
+        assert_eq!(bar + qux, Foo(3));
     }
 }
