ed017e9df2fe9da6d43a8f02b38e59d6ccdfd98e
bstrie <bstrie@thin>
2019-04-15 21:12:41 -0400

    Foolishness

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,18 @@ use quote::quote;
 
 #[proc_macro_attribute]
 pub fn hello(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("{:?}", attr);
+    println!("{:?}", item);
     let input = syn::parse_macro_input!(item as syn::ItemFn);
     let name = &input.ident;
 
     // Our input function is always equivalent to returning 42, right?
     let result = quote! {
-        fn #name() -> u32 { 42 }
+        const FOO: i32 = 42;
     };
-    result.into()
+    let result: TokenStream = result.into();
+    println!("{:?}", result);
+    result
 }
 
 
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -3,5 +3,5 @@ fn wrapped_function() {}
 
 #[test]
 fn works() {
-    assert_eq!(42, wrapped_function());
+    assert_eq!(42, FOO);
 }
