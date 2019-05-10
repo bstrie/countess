6115d4cb9da0d6d82d4957b13df4a7ff5533d7bd
bstrie <bstrie@thin>
2019-05-10 14:29:32 -0400

    Tweak range parsing

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
-    Expr, ExprLit, ExprRange, ExprUnary, ItemStruct, Lit, RangeLimits, Result, Token, UnOp, parse_macro_input,
+    Expr, ExprLit, ExprRange, ExprUnary, ItemStruct, ItemTrait, Lit, RangeLimits, Result, Token, UnOp, parse_macro_input,
     parse::{Parse, ParseStream},
     punctuated::Punctuated,
 };
@@ -16,7 +16,7 @@ trait SimplifyLimit {
 
 // The limits of a range can be any expression;
 // currently we only support integer literals as limits
-// (otherwise we would need to depend on Miri...)
+// (otherwise we would need to, uh, depend on Miri or something...)
 // so extract the number that the literal represents,
 // and produce a compiler error on any more complex expression.
 impl SimplifyLimit for Option<Box<Expr>> {
@@ -33,7 +33,7 @@ impl SimplifyLimit for Option<Box<Expr>> {
                 num.value() as i128
             },
             Expr::Unary(ExprUnary { op: UnOp::Neg(_), expr, .. }) => -Self::extract(*expr),
-            _ => panic!("Non-literals are not supported")
+            _ => panic!("Expressions other than numeric literals are not currently supported")
         }
     }
 }
@@ -49,13 +49,19 @@ impl SimplifyLimit for Option<Box<Expr>> {
 // cannot be determined until we parse the underlying type,
 // so at this stage we use a None to represent an omitted bound.
 #[derive(Debug)]
-enum Range {
-    Closed(Option<i128>, Option<i128>),
-    Open(Option<i128>, Option<i128>)
+enum Interval { Open, Closed }
+
+#[derive(Debug)]
+struct Range {
+    lower: Option<i128>,
+    upper: Option<i128>,
+	interval: Interval,
 }
 
 #[derive(Debug)]
-struct RangeSeq(Vec<Range>);
+struct RangeSeq {
+	ranges: Vec<Range>
+}
 
 // Implementing syn::Parse is how syn allows one to
 // hook into the syn::parse_macro_input! macro.
@@ -67,15 +73,18 @@ impl Parse for RangeSeq {
         let ranges_ast: Punctuated<ExprRange, Token![,]> = input.parse_terminated(ExprRange::parse)?;
 
         // turn the ExprRanges into something simpler, our own Range enum
-        let mut ranges = RangeSeq(Vec::new());
+        let mut rseq = RangeSeq { ranges: Vec::new() };
         for ExprRange {limits, from, to, ..} in ranges_ast {
-            let interval = match limits {
-                RangeLimits::HalfOpen(_) => Range::Open,
-                RangeLimits::Closed(_) => Range::Closed
-            };
-            ranges.0.push(interval(from.simple(), to.simple()));
+			rseq.ranges.push(Range {
+				lower: from.simple(),
+				upper: to.simple(),
+				interval: match limits {
+					RangeLimits::HalfOpen(_) => Interval::Open,
+					RangeLimits::Closed(_) => Interval::Closed
+				}
+            });
         }
-        Ok(ranges)
+        Ok(rseq)
     }
 }
 
@@ -88,9 +97,9 @@ pub fn range(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemStruct);
     let name = &input.ident;
 
-    let (lower, upper) = match pattr.0[0] {
-        Range::Closed(l, u) | Range::Open(l, u) => (l.unwrap() as i32, u.unwrap() as i32)
-    };
+	let (lower, upper) = match pattr.ranges[0] {
+		Range {lower, upper, ..} => (lower.unwrap() as i32, upper.unwrap() as i32)
+	};
     // TODO: We're gonna need hygiene SOMEWHERE in here, right?
     let result = quote! {
         struct #name(i32);
@@ -103,7 +112,11 @@ pub fn range(attr: TokenStream, item: TokenStream) -> TokenStream {
     //println!("--- result ---\n{:?}\n", result);
     result
 }
-
+/*
+#[prac_macro_attribute]
+pub fn make_int(attr: TokenStream, item:TokenStream) -> TokenStream {
+	let pinput = parse_macro_input!(item as ItemTrait);
+*/
 
 #[cfg(test)]
 mod tests {
